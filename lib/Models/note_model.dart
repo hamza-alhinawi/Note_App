e129b69731700ed99720dac346bbb00d4df171e1
hamza-alhinawi <hamzaalhinawi33@gmail.com>
2025-11-11 01:39:13 +0200

    create note modle

---
+++ lib/Models/note_model.dart
@@ -0,0 +1,12 @@
+class NoteModel {
+  final String title;
+  final String content;
+  final DateTime date;
+  final int color;
+  NoteModel({
+    required this.color,
+    required this.title,
+    required this.content,
+    required this.date,
+  });
+}
